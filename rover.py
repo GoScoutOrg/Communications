bd077043bf9d8cf622b44820c5eacab95545c24d
samuelsehnert <samuel.sehnert@gmail.com>
2022-10-25 09:03:13 -0700

    jkfjdsfksdjf

--- rover.py
+++ rover.py
@@ -30,7 +30,7 @@ while inputs:
             print(addr, "just joined")
             connection.setblocking(0)
             inputs.append(connection)
-            if messageQueues[connection.getpeername()[0]]:
+            if messageQueues.get(connection.getpeername()[0]):
                 print('reconnect')
             messageQueues[connection.getpeername()[0]] = [queue.Queue(), 0]
         else:
